c03cfbb96ffe05553e2955fcdf93555f75c38411
Serena Dean <dserena01@yahoo.com>
2022-12-25 04:19:55 -0600

    Update firebase credentials

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ firebase.initializeApp({
     "project_id": process.env.FIREBASE_PROJECT_ID,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
     "token_uri":"https://oauth2.googleapis.com/token"
     })
 });
