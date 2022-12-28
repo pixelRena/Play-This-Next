9f38cbd7c3bbb7b45350480233f18d5e9a52990d
Serena Dean <dserena01@yahoo.com>
2022-12-28 17:20:08 -0600

    Updating server for vercel testing

--- index.js
+++ index.js
@@ -115,3 +115,5 @@ app.post('/add-suggested-game', async (req,res) => {
 app.use('/', (req,res) => res.sendFile(path.join(staticPath, "index.html")));
 
 app.listen(process.env.PORT, () => { console.log("listening on port.."+process.env.PORT)});
+
+module.exports = app;
